134554188961b35623f9f5a4f2811140fc5120c6
Jozuo <jozuo.dev@gmail.com>
2023-07-20 15:29:57 +0900

    5章: Chirpsの更新

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,7 @@ Route::get('/', function () {
 });
 
 Route::resource('chirps', ChirpController::class)
-->only(['index', 'store'])
+->only(['index', 'store', 'update'])
 ->middleware(['auth', 'verified']);
 
 Route::get('/dashboard', function () {
